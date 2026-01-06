ba38a645897d47cc78e6be00e6a5e97a10690fa8
lucian.sander <Sanderlu@icloud.com>
2026-01-05 22:08:20 -0300

    feat: criando rotas

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\LocacaoController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ModeloController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/', function () {
-    return ["usando api"];
-});
+Route::apiResource('cliente', ClienteController::class);
+Route::apiResource('carro', CarroController::class);
+Route::apiResource('locacao', LocacaoController::class);
+Route::apiResource('marca', MarcaController::class);
+Route::apiResource('modelo', ModeloController::class);
