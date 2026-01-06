7c6412a141cb8c01cf76260dc48ca89caf445e5e
lucian.sander <Sanderlu@icloud.com>
2026-01-05 22:09:43 -0300

    feat: crud ok

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -12,23 +12,29 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        $data = Marca::all();
+
+        return response()->json($data, 200);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    // public function create()
+    // {
+    //     //
+    // }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        Marca::create($request->all());
+
+        return response()->json([
+            'msg' => 'Salvo Com Sucesso!'
+        ], 200);
     }
 
     /**
@@ -36,23 +42,31 @@ class MarcaController extends Controller
      */
     public function show(Marca $marca)
     {
-        //
+        $data = Marca::find($marca->id);
+
+        return response()->json([
+            $data
+        ], 200);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Marca $marca)
-    {
-        //
-    }
+    // public function edit(Marca $marca)
+    // {
+    //     //
+    // }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->update($request->all());
+
+        return response()->json([
+            'msg' => 'Atualizado Com Sucesso!'
+        ], 200);
     }
 
     /**
@@ -60,6 +74,10 @@ class MarcaController extends Controller
      */
     public function destroy(Marca $marca)
     {
-        //
+        $marca->delete();
+
+        return response()->json([
+            'msg' => 'Deletado Com Sucesso!'
+        ], 200);
     }
 }
