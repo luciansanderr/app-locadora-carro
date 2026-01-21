cc5ca7b8d62936f55cb29ecfb23fe40f3e4b44da
lucian.sander <Sanderlu@icloud.com>
2026-01-21 18:23:03 -0300

    feat: Ajuste de validação e protected follable

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Modelo extends Model
 {
-    //
+    protected $fillable = ['marca_id', 'nome', 'imagem', 'numero_portas', 'lugares', 'air_bag', 'abs'];
+
+    public function rules() {
+        return [
+            'marca_id' => 'required|exists:marcas,id',
+            'nome' => [
+                'required',
+                'min:3',
+                Rule::unique('modelos', 'nome')->ignore($this->id)
+            ],
+            'imagem' => 'required|file|mimes:png|max:2048',
+            'numero_portas'=> 'required|integer|digits_between:1,5',
+            'lugares'=> 'required|integer|digits_between:1,9',
+            'air_bag'=> 'required|boolean',
+            'abs'=> 'required|boolean',
+        ];
+    }
+
+    public function feedback() {
+        return [
+            'required' => 'O campo :attribute é obrigatório!',
+            // 'nome.unique' => 'O nome da marca já existe!',
+            // 'nome.min' => 'O nome da marca deve ter no mínimo 3 caracteres!',
+            // 'imagem.mimes' => 'A imagem deve ser do tipo PNG!',
+            // 'imagem.max' => 'A imagem deve ter no máximo 2MB!',
+            // 'imagem.file' => 'A imagem deve ser um arquivo válido!',
+        ];
+    }
 }
