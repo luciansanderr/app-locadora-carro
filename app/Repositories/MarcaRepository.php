737b72829f7b29a521678a11b930d9791369b0b3
lucian.sander <Sanderlu@icloud.com>
2026-02-02 16:32:32 -0300

    feat: criando um repository para lógica

---
+++ app/Repositories/MarcaRepository.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MarcaRepository
+{
+    public function __construct(Model $model) {
+        $this->model = $model;
+    }
+
+    public function selectAtributosModelo($atributos)
+    {
+        $this->model = $this->model->with($atributos);
+    }
+
+    public function selectAtributosMarca($atributos)
+    {
+        $this->model = $this->model->selectRaw($atributos);
+    }
+
+    public function filtroWhere($atributos) {
+        $filtro = explode(';', $atributos);
+        foreach ($filtro as $key => $condicao) {
+            $c = explode(':', $condicao);
+        }
+        $this->model = $this->model->where($c[0], $c[1],  $c[2]);
+    }
+
+    public function getResultado()
+    {
+        return $this->model->get();
+    }
+}
