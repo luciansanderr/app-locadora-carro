132d9a789a5fad33f6f567779636ab17e7bfb946
lucian.sander <Sanderlu@icloud.com>
2026-02-03 20:58:48 -0300

    feat: Crianção de cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -5,15 +5,51 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreClienteRequest;
 use App\Http\Requests\UpdateClienteRequest;
 use App\Models\Cliente;
+use App\Repositories\ClienteRepository;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\Utils\Util;
 
 class ClienteController extends Controller
 {
+    public function __construct(Cliente $cliente) {
+        $this->cliente = $cliente;
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $clienteRepository = new ClienteRepository($this->cliente);
+        //$data = carro::all();
+        $atributos = [];
+        $atributosModelo = [];
+
+        // if ($request->has('atributos_modelo')) {
+        //     $atributosModelo = "modelo:id,". $request->atributos_modelo;
+        //     $clienteRepository->selectAtributosRelacionados($atributosModelo);
+        // }
+
+        // if (!$request->has('atributos_modelo')) {
+        //     $clienteRepository->selectAtributosRelacionados('modelo');
+        // }
+
+        if ($request->has('atributos')) {
+            $atributos = $request->atributos;
+            $clienteRepository->selectAtributos($atributos);
+        }
+
+        if ($request->has('filtro')) {
+            $atributos = $request->filtro;
+            $clienteRepository->filtroWhere($atributos);
+        }
+
+        $data = $clienteRepository->getResultado();
+
+        if (empty($data)) {
+            return response()->json(['msg' => 'Não Encontrado'], 404);
+        }
+        return response()->json($data, 200);
     }
 
     /**
@@ -27,17 +63,30 @@ class ClienteController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreClienteRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->cliente->rules(), $this->cliente->feedback());
+        try {
+            $data = $this->cliente->create([
+                'nome' => $request->nome
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json(['msg' => 'Ocorreu um Erro!', 'data' => $th->getMessage()], 404);
+        }
+
+        return response()->json(['msg' => 'Salvo Com Sucesso!', 'data' => $data], 200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Cliente $cliente)
+    public function show($id)
     {
-        //
+        $data = $this->cliente->find($id);
+        if (empty($data)) {
+            return response()->json(['msg' => 'Não Encontrado'], 404);
+        }
+        return response()->json([$data], 200);
     }
 
     /**
@@ -51,16 +100,34 @@ class ClienteController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateClienteRequest $request, Cliente $cliente)
+    public function update(Request $request, $id)
     {
-        //
+        $data = $this->cliente->find($id);
+        if (empty($data)) {
+            return response()->json(['msg' => 'Não Encontrado'], 404);
+        }
+        if ($request->method() === Util::PATCH) {
+            $request->validate(Util::regrasDinamicas($request, $this->cliente));
+        }
+        if ($request->method() === Util::PUT) {
+            $request->validate($this->cliente->rules());
+        }
+        $data->update([
+            'nome'=> $request->nome
+        ]);
+        return response()->json(['msg' => 'Atualizado Com Sucesso!', 'data' => $data], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Cliente $cliente)
+    public function destroy($id)
     {
-        //
+        $data = $this->cliente->find($id);
+        if (empty($data)) {
+            return response()->json(['msg' => 'Não Encontrado'], 404);
+        }
+        $data->delete();
+        return response()->json(['msg' => 'Deletado Com Sucesso!'], 200);
     }
 }
--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -4,9 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Cliente extends Model
 {
     /** @use HasFactory<\Database\Factories\ClienteFactory> */
     use HasFactory;
+
+    protected $fillable = ['id', 'nome'];
+
+    public function rules() {
+        return [
+            'nome' => [
+                'required',
+                'min:3',
+                Rule::unique('clientes', 'nome')->ignore($this->id)
+            ]
+        ];
+    }
+
+    public function feedback() {
+        return [
+            'required' => 'O campo :attribute é obrigatório!',
+            // 'nome.unique' => 'O nome da marca já existe!',
+            // 'nome.min' => 'O nome da marca deve ter no mínimo 3 caracteres!',
+            // 'imagem.mimes' => 'A imagem deve ser do tipo PNG!',
+            // 'imagem.max' => 'A imagem deve ter no máximo 2MB!',
+            // 'imagem.file' => 'A imagem deve ser um arquivo válido!',
+        ];
+    }
 }
